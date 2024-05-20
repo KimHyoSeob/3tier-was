9db79fcbf2638b37f7ed4b45a24f62ae4d141be1
KHS <121760344+KimHyoSeob@users.noreply.github.com>
2024-05-21 08:21:52 +0900

    Update settings.py

--- khs_3tier/settings.py
+++ khs_3tier/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'khs_3tier.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'myd',           # 데이터베이스 이름
-    #     'USER': 'mydatabaseuser',       # MySQL 사용자 이름
-    #     'PASSWORD': 'mypassword',       # MySQL 비밀번호
-    #     'HOST': 'localhost',            # 
-    #     'PORT': '3306',                 # MySQL 포트
-    # }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'mydb',           # 데이터베이스 이름
+         'USER': 'dbuser',       # MySQL 사용자 이름
+         'PASSWORD': 'dbpassword',       # MySQL 비밀번호
+         'HOST': '$DB_ENDPOINT',            # DB 엔드포인트
+         'PORT': '3306',                 # MySQL 포트
+     }
 }
 
 
